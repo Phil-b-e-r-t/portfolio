188d89edadb7faca1e1571fcafe90ea57b796fe1
Phil-b-e-r-t <ampuriraphilbert@gmail.com>
2026-07-19 11:41:26 +0300

    Updating the Gmail password

--- config.py
+++ config.py
@@ -5,10 +5,17 @@ load_dotenv()
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config:
 
+    # -----------------------------
+    # Flask
+    # -----------------------------
     SECRET_KEY = os.getenv("SECRET_KEY")
 
+    # -----------------------------
+    # Database
+    # -----------------------------
     SQLALCHEMY_DATABASE_URI = (
         f"mysql+pymysql://{os.getenv('DB_USER')}:"
         f"{os.getenv('DB_PASSWORD')}@"
@@ -18,22 +25,31 @@ class Config:
     )
 
     SQLALCHEMY_ENGINE_OPTIONS = {
-    "pool_pre_ping": True,
-    "pool_recycle": 300,
-    "connect_args": {
-        "ssl": {
-            "ca": os.path.join(BASE_DIR, "certs", "ca.pem")
+        "pool_pre_ping": True,
+        "pool_recycle": 300,
+        "connect_args": {
+            "ssl": {
+                "ca": os.path.join(BASE_DIR, "certs", "ca.pem")
+            }
         }
     }
-}
-
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    # -----------------------------
+    # Mail
+    # -----------------------------
     MAIL_SERVER = "smtp.gmail.com"
     MAIL_PORT = 587
+
     MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
 
     MAIL_USERNAME = os.getenv("MAIL_USERNAME")
     MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
     MAIL_DEFAULT_SENDER = os.getenv("MAIL_USERNAME")
+
+    MAIL_SUPPRESS_SEND = False
+    MAIL_MAX_EMAILS = 1
+    MAIL_ASCII_ATTACHMENTS = False
+    MAIL_TIMEOUT = 10
