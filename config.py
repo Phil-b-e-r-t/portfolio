0238f60c906ffb792295a9d530df494ce77e60e8
Phil-b-e-r-t <ampuriraphilbert@gmail.com>
2026-07-19 16:04:10 +0300

    Change in app.py

--- config.py
+++ config.py
@@ -39,14 +39,17 @@ class Config:
     # -----------------------------
     # Mail
     # -----------------------------
-    MAIL_SERVER = "smtp-relay.brevo.com"
+    MAIL_SERVER = "smtp.gmail.com"
     MAIL_PORT = 587
+
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
 
     MAIL_USERNAME = os.getenv("MAIL_USERNAME")
     MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
-
     MAIL_DEFAULT_SENDER = os.getenv("MAIL_USERNAME")
 
+    MAIL_SUPPRESS_SEND = False
+    MAIL_MAX_EMAILS = 1
+    MAIL_ASCII_ATTACHMENTS = False
     MAIL_TIMEOUT = 10
